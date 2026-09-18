f98909c51e75fd58af6d0eb6c9a41f7b699d0f00
laknes <rezajanami@gmail.com>
2026-09-18 19:03:13 +0330

    fix bugs

--- src/app/[lang]/admin/settings/[group]/page.tsx
+++ src/app/[lang]/admin/settings/[group]/page.tsx
@@ -1,4 +1,4 @@
-import { AdminSettingsPage } from '../page';
+import { AdminSettingsPage } from '../_components/AdminSettingsPage';
 
 export default async function AdminSettingsGroupPage({ params }: { params: Promise<{ group: string }> }) {
   const { group } = await params;
